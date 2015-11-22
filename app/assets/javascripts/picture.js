b6223228bf7ba4b9233fc30151df9d5f3142a498
jeffslutzky <jeffslutzky@gmail.com>
2015-11-21 20:44:46 -0500

    added photo borders on radio button click

--- app/assets/javascripts/picture.js
+++ app/assets/javascripts/picture.js
@@ -1,9 +1,19 @@
 $(function(){
 
-   $('#first-pic').on('click', function ()
+  $('#first-pic').on('click', function ()
    {
        $(this).width(500);
-       
    });
+
+
+  $("input:radio").click(function() {
+        var img_id = $("input[name=id]:checked").val()
+        $("#"+img_id).addClass("photo-border");
+    });
+
+  $("input:radio").blur(function() {
+        var img_id = $("input[name=id]:checked").val()
+        $("#"+img_id).removeClass("photo-border");
+    });
 
 });
