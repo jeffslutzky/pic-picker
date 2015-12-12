6d5c2806fdc6d650aa338b3f0d726ab9f23b1dae
jeffslutzky <jeffslutzky@gmail.com>
2015-12-11 23:06:38 -0500

    email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,6 @@ class UserMailer < ApplicationMailer
 	def result_email(user)
 		@user = user
 		@url  = user.photo_url
-		binding.pry
     mail(to: @user.email_address, subject: 'Your favorite photo')
 	end
 
