3bc33d628dba09da13756cffbddff99206fc1219
jeffslutzky <jeffslutzky@gmail.com>
2015-12-17 21:47:59 -0500

    adjust js

--- app/assets/javascripts/picture.js
+++ app/assets/javascripts/picture.js
@@ -1,11 +1,5 @@
 $(function(){
 
-  $('#first-pic').on('click', function ()
-   {
-       $(this).width(500);
-   });
-
-
   $("input:radio").click(function() {
         var img_id = $("input[name=id]:checked").val()
         $("#"+img_id).addClass("photo-border");
